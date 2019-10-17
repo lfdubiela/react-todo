411d93ba6b4a974e36e1e1e769b3bf939f1d0b26
lfdux <lfdubiela@gmail.com>
2019-10-17 10:00:48 -0300

    1 commit - React todo

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+
+export default props => {
+    return (
+        <div role='form' className='todo-form'>
+            <Grid cols='12 9 10'>
+                <input id="description" type="text" className="form-control"
+                    placeholder='Adicione uma tarefa'
+                    onChange={props.handleChange}
+                    value={props.description}
+                />
+            </Grid>
+
+            <Grid cols='12 3 2'>
+                <IconButton style='primary' icon='plus' onClick={props.handleAdd} />
+                <IconButton style='info' icon='search' onClick={props.handleSearch} />
+                <IconButton style='default' icon='close' onClick={props.handleClear} />
+            </Grid>
+        </div>
+    )
+}
